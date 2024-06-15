e9ba81c973e5dfdc3ef551eb3de5fd9fe06af48a
Wanderson Guedes <wg21249@gmail.com>
2024-06-14 22:52:49 -0300

    api_criada

---
+++ src/main/java/com/api_arcade/api_arcade/ApiArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.api_arcade.api_arcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_arcade/api_arcade/ApiArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_arcade.api_arcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
